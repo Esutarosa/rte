6f969f6ce3c6a9aaa02c1fd97c81319c75c70027
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-04-18 23:44:02 +0300

    + Splitting an element by characters

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use std::io;
+use std::io::{self, Read};
+use termion::raw::IntoRawMode;
 
 fn main() {
+    let _raw_terminal = io::stdout().into_raw_mode().unwrap();
     let stdin = io::stdin();
-    let mut buf = String::new();
-    stdin.read_line(&mut buf).unwrap();
-    println!("Your message: {}", buf);
+    let bytes_iter = stdin.bytes();
+    for byte in bytes_iter {
+        let byte = byte.unwrap();
+        let c = byte as char;
+        if c == 'q' {
+            break;
+        }
+        println!("{} {}\r", c, byte);
+    }
 }

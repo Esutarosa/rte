2e41b8da5efffca0a5bac079a270fb4b24b7e73d
Esutarosa <esutarosa.git@gmail.com>
2024-04-21 01:09:44 +0300

    Editor implementation

--- src/editor.rs
+++ src/editor.rs
@@ -5,3 +5,24 @@ pub struct Editor {
     exit: bool,
     stdout: RawTerminal<Stdout>,
 }
+
+impl Editor {
+    pub fn new() -> Result<Self, io::Error> {
+        Ok(Editor {
+            exit: false,
+            stdout: io::stdout().into_raw_mode()?,
+        })
+    }
+
+    pub fn run(&mut self) -> Result<(), io::Error> {
+        while !self.exit {
+            println!("Please input key!\r");
+            self.process_key()?;
+        }
+
+        println!("goodbye!\r");
+        self.stdout.flush();
+    }
+
+    fn process_key(&mut self) -> Result<(), io::Error> {}
+}
